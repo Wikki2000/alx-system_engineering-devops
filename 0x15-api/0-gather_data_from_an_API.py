6f67225adfe3d571d81478514baea12bb022e92d
wikki2000 <wisdomokposin@gmail.com>
2024-06-16 19:06:45 +0100

    Add files

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Returns to-do list information for a given employee ID.
+Usage: python3 <script_name.py> <employee_id>
+"""
+import requests
+import sys
+
+
+def fetch_user_data(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    try:
+        # Fetch user information
+        user_response = requests.get(f"{base_url}/users/{employee_id}")
+        user_response.raise_for_status()
+        user = user_response.json()
+
+        # Fetch TODO list for the user
+        todos_response =  requests.get(f"{base_url}/todos/", params={"userId": employee_id})
+        todos_response.raise_for_status() # Get response status
+        todos = todos_response.json() # Parse the value
+
+        return user, todos
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        sys.exit(1)
+
+def display_todo_progress(user, todos):
+    total_tasks = 0
+    count_completed_tasks = 0
+    list_of_completed_tasks = []
+
+    for task in todos:
+        total_tasks += 1
+        if task["completed"] == True:
+            count_completed_tasks += 1
+            list_of_completed_tasks.append(task["title"])
+
+            print(f"User {user['name']} has {total_tasks} tasks in total.")
+
+if  __name__ == "__main__":
+     # Verify correct usage
+    if len(sys.argv) != 2:
+        print("Usage: python3 <script_name> <employee_id>")
+        sys.exit(1)
+    employee_id = sys.argv[1]
+    todos, user = fetch_user_data(employee_id)
+    display_todo_progress(user, todos)
