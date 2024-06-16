e2f404fc418375c9b601428bcd6a8dd60fda070e
wikki2000 <wisdomokposin@gmail.com>
2024-06-16 11:10:21 -0700

    Resolve bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,6 @@ Usage: python3 <script_name.py> <employee_id>
 import requests
 import sys
 
-
 def fetch_user_data(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
     try:
@@ -16,11 +15,12 @@ def fetch_user_data(employee_id):
         user = user_response.json()
 
         # Fetch TODO list for the user
-        todos_response =  requests.get(f"{base_url}/todos/", params={"userId": employee_id})
-        todos_response.raise_for_status() # Get response status
-        todos = todos_response.json() # Parse the value
+        todos_response = requests.get(f"{base_url}/todos/", params={"userId": employee_id})
+        todos_response.raise_for_status()
+        todos = todos_response.json()
 
         return user, todos
+
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         sys.exit(1)
@@ -30,19 +30,22 @@ def display_todo_progress(user, todos):
     count_completed_tasks = 0
     list_of_completed_tasks = []
 
+
     for task in todos:
         total_tasks += 1
         if task["completed"] == True:
             count_completed_tasks += 1
             list_of_completed_tasks.append(task["title"])
 
-            print(f"User {user['name']} has {total_tasks} tasks in total.")
+    print(f"User {user['name']} has {total_tasks} tasks in total.")
+    print(f"Completed {count_completed_tasks} tasks.")
 
-if  __name__ == "__main__":
-     # Verify correct usage
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 <script_name> <employee_id>")
+        print("Usage: python3 <script_name.py> <employee_id>")
         sys.exit(1)
+
     employee_id = sys.argv[1]
-    todos, user = fetch_user_data(employee_id)
+    user, todos = fetch_user_data(employee_id)
     display_todo_progress(user, todos)
+
