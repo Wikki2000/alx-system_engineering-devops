37e5f1e25d6fa25826b90934d1249c523f9799b8
wikki2000 <wisdomokposin@gmail.com>
2024-06-28 18:37:52 +0100

    Update 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-# Installs Flask and Werkzeug
-
+# Install Flask and Werkzeug
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
