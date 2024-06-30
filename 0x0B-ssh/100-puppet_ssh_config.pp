f477208993a96c0d410bf7eb01e8721170828078
wikki2000 <wisdomokposin@gmail.com>
2024-07-01 00:51:43 +0100

    Add 2-ssh_config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+exec { 'add_identityfile_to_ssh_config':
+  command => 'echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  provider => shell,
+}
+
+exec { 'turn_off_password_authentication':
+  command => 'sed -i "s/^# *PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/ssh_config',
+  provider => shell,
+}
+
