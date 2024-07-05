519ca0823a07883ad617c68ee59350044a8881fb
wikki2000 <wisdomokposin@gmail.com>
2024-07-05 12:57:15 +0100

    add: 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,46 +1,57 @@
-# Ensured nginx package is installed
-package { 'nginx':
-    ensure    => installed
+# Install Nginx web server (w/ Puppet)
+# Nginx should be listening on port 80
+# it must return a page that contains the string Hello World! when queried with
+# curl in /
+# The redirection must be a “301 Moved Permanently”
+
+# ensure nginx package resource is installed
+package {'nginx':
+ensure  => installed,
 }
 
-# Define content for root
-file { '/var/www/html/index.html':
-    content    => 'Hello World!',
-    ensure     => install,
+# create a file resource for index html
+file {'/var/www/html/index.html':
+ensure  => present,
+content => 'Hello World!',
 }
 
-# Configure Nginx default site
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
-  root /var/www/html;
-  index index.html index.htm;
-
-  location /redirect_me {
-    return 301 https://youtube.com/;
-  }
-
-  error_page 404 /404;
-  location /404 {
-    internal;
-    root /var/www/html;
-  }
-}
-",
+# create a file for 404 error page
+file {'/var/www/html/error_404.html':
+ensure   => present,
+content  => "Ceci n'est pas une page",
 }
 
-# Enable default site by creating a symlink
+# create a default sites for nginx
+file {'/etc/nginx/sites-available/default':
+ensure  => present,
+content => "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm;
+
+	location /redirect_me {
+		return 301 https://youtube.com/;
+	}
+
+	error_page 404 /error_404.html;
+	location /404 {
+		root /var/www/html;
+		internal;
+	}
+    }",
+require => Package['nginx'],
+notify  => Service['nginx'],
+}
+
 file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
+ensure   => link,
+target   => '/etc/nginx/sites-available/default',
+notify   => Service['nginx'],
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-} 
+service {'nginx':
+ensure    => running,
+enable    => true,
+subscribe => File['/etc/nginx/sites-enabled/default'],
+}
