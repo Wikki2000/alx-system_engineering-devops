7db3bb987c278794495e7e8901d9a5eb089cf579
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-07-14 08:35:51 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,16 +3,16 @@ exec { 'update':
   provider => shell,
 }
 
-exec { 'install':
+-> exec { 'install':
   command  => 'sudo apt-get -y install nginx',
   provider => shell,
 }
 
-exec { 'X-Served-By':
+-> exec { 'X-Served-By':
   command  => 'sudo sed -i "/listen 80 default_server;/a         add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
-exec { 'service -y nginx':
+-> exec { 'service -y nginx':
   command => '/usr/sbin/service nginx restart',
 }
