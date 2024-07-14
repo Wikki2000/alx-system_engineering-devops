ad886d0b4cbde822ea63d2cec79320b2af06502c
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-07-14 10:09:25 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,8 +12,7 @@ exec { 'update':
 
 # adds the header to the file
 -> exec { 'X-Served-By':
-  command  => 'sudo sed -i "/listen 80 default_server;/a         add_header X-Served-By $(hostname);
-" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
