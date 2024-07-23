f00182c6a8756cba249b66e12bb7f61d40e7ef1e
wikki2000 <wisdomokposin@gmail.com>
2024-07-23 20:46:14 +0100

    add: 0-install_mysql_v5.py

---
+++ 0x14-mysql/0-install_mysql_v5.py
@@ -0,0 +1,3 @@
+#!/usr/env bash
+# Instal mysql version 5
+sudo wget -O mysql57 https://raw.githubusercontent.com/nuuxcode/alx-system_engineering-devops/master/scripts/mysql57 && sudo chmod +x mysql57 &&  sudo ./mysql57
